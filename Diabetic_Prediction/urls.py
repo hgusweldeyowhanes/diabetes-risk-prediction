df76e28df291d3a81ec9838be0a00550213dc6ca
Hgus Weldeyowhanes <hgus.w@convexit.net>
2025-09-30 11:05:55 +0300

    update urls

--- Diabetic_Prediction/urls.py
+++ Diabetic_Prediction/urls.py
@@ -1,12 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.home, name='home'),
-#     path('predict/', views.DiabetesPredictionView.as_view(), name='predict_diabetes'),
-#     path('api/predict/', views.api_predict_diabetes, name='api_predict_diabetes'),
-#     path('health/', views.health_check, name='health_check'),
-# ]
 from django.urls import path
 from . import views
 
